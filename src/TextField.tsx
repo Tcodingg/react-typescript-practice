7df0d19c2fc2560c8e8eda7fbff323f959792cef
DePrincio <princetlive@yahoo.com>
2021-05-17 18:16:10 -0400

    type script propss

--- src/TextField.tsx
+++ src/TextField.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
+interface person {
+	firstName: string;
+	lastName: string;
+}
+
 interface Props {
 	text: string;
-	ok: boolean;
-	i: number;
-	fn: () => void;
+	ok?: boolean;
+	age?: number;
+	fn?: (name: string) => string;
+	person: person;
 }
 
 export const TextField: React.FC<Props> = () => {
